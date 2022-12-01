5772599d87e62839259fd71d268c5e8e531b795f
ibrahimhakkihelvaci <66227589+ibrahimhakkihelvaci@users.noreply.github.com>
2022-12-02 00:41:04 +0300

    create products header

---
+++ src/components/ProductsHeader.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="products-header">
+    <div class="products-header__content">
+      <div class="products-header__count">{{ productCount }} Products</div>
+      <Dropdown
+        title="Sort By: Status"
+        :items="dropdownItems"
+        @onClickItem="handleClickDropdownItem"
+      />
+    </div>
+    <div class="products-header__action">
+      <Button label="Add new product" @onClick="handleClickButton" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { IDropdownItem } from "../utils/types";
+
+interface Props {
+  productCount: string;
+}
+
+defineProps<Props>();
+
+const emit = defineEmits(["onChangeSortType", "onClickAddNewProduct"]);
+
+const dropdownItems = [
+  {
+    label: "In Progress",
+    value: "in-progress",
+  },
+  {
+    label: "Completed",
+    value: "completed",
+  },
+  {
+    label: "Pending",
+    value: "pending",
+  },
+];
+
+const handleClickDropdownItem = (item: IDropdownItem) => {
+  emit("onChangeSortType", item);
+};
+
+const handleClickButton = () => {
+  emit("onClickAddNewProduct");
+};
+</script>
+
+<style lang="scss">
+.products-header {
+  background: rgba(255, 255, 255, 0.05);
+  border: 1px solid rgba(255, 255, 255, 0.05);
+  border-radius: 16px;
+  padding: 13px 16px 14px 24px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  &__content {
+    display: flex;
+    align-items: center;
+  }
+
+  &__count {
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 20px;
+    color: #fff;
+    margin-right: 16px;
+  }
+}
+</style>
