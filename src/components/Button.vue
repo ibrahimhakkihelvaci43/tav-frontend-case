d51497eb02d5099db6a8b78eeb6ad98ad4674a05
ibrahimhakkihelvaci <66227589+ibrahimhakkihelvaci@users.noreply.github.com>
2022-12-02 19:49:44 +0300

    add button variant

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -10,7 +10,7 @@
 <script setup lang="ts">
 interface Props {
   label: string;
-  type?: "primary" | "secondary";
+  type?: "primary" | "secondary" | "link";
 }
 
 withDefaults(defineProps<Props>(), {
@@ -19,7 +19,7 @@ withDefaults(defineProps<Props>(), {
 
 const emit = defineEmits(["onClick"]);
 </script>
- 
+
 <style lang="scss">
 .button-base {
   height: 40px;
@@ -32,6 +32,10 @@ const emit = defineEmits(["onClick"]);
   line-height: 18px;
   padding: 11px;
 
+  &:focus{
+    outline: none;
+  }
+
   &--primary {
     background-color: #c6c7f8;
     color: #1c1c1c;
@@ -42,6 +46,16 @@ const emit = defineEmits(["onClick"]);
     background-color: rgba(255, 255, 255, 0.05);
     color: #fff;
     border: 1px solid rgba(255, 255, 255, 0.1);
+  }
+
+  &--link {
+    height: unset;
+    background-color: unset;
+    font-weight: 400;
+    font-size: 12px;
+    color: #b2b3df;
+    padding: 0;
+    min-width: unset;
   }
 }
 </style>
