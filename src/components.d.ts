43d4ddbc1396d49b422c86b7e909cd9ebcecb534
ibrahimhakkihelvaci <66227589+ibrahimhakkihelvaci@users.noreply.github.com>
2022-12-02 13:50:46 +0300

    create modal

--- src/components.d.ts
+++ src/components.d.ts
@@ -9,11 +9,12 @@ declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     Button: typeof import('./components/Button.vue')['default']
     Checkbox: typeof import('./components/Checkbox.vue')['default']
-    copy: typeof import('./components/StatusTag copy.vue')['default']
+    copy: typeof import('./components/Input copy.vue')['default']
     Dropdown: typeof import('./components/Dropdown.vue')['default']
     HelloWorld: typeof import('./components/HelloWorld.vue')['default']
     Input: typeof import('./components/Input.vue')['default']
     MenuItem: typeof import('./components/MenuItem.vue')['default']
+    Modal: typeof import('./components/Modal.vue')['default']
     ProductItem: typeof import('./components/ProductItem.vue')['default']
     ProductsHeader: typeof import('./components/ProductsHeader.vue')['default']
     Select: typeof import('./components/Select.vue')['default']
---
+++ src/components/Modal.vue
@@ -0,0 +1,75 @@
+<template>
+  <div v-if="visible" class="modal">
+    <div class="modal__content">
+      <div class="modal__title">
+        {{ title }}
+      </div>
+
+      <slot />
+
+      <Icon
+        class="modal__close"
+        name="Close"
+        width="24"
+        height="24"
+        fill="#ffffff66"
+        @click="onClose"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Icon from "../utils/Icon";
+
+interface Props {
+  title: string;
+  visible: boolean;
+}
+
+defineProps<Props>();
+
+const emit = defineEmits(["update:visible"]);
+
+const onClose = () => {
+  emit("update:visible", false);
+};
+</script>
+
+<style lang="scss">
+.modal {
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  background-color: rgba(0, 0, 0, 0.5);
+
+  &__title {
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 20px;
+    margin-bottom: 20px;
+  }
+
+  &__content {
+    width: 445px;
+    position: relative;
+    background: #160d1c;
+    border: 1px solid rgba(255, 255, 255, 0.05);
+    border-radius: 16px;
+    padding: 24px 32px 32px 24px;
+  }
+
+  &__close {
+    position: absolute;
+    right: 24px;
+    top: 24px;
+    cursor: pointer;
+  }
+}
+</style>
