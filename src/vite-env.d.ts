3802f144c382761a5a3f9c7610955a5931efe51c
ibrahimhakkihelvaci <66227589+ibrahimhakkihelvaci@users.noreply.github.com>
2022-12-04 19:31:01 +0300

    delete types.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,19 @@
 /// <reference types="vite/client" />
 
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
 }
+
+declare module "*.svg?inline" {
+  const content: any;
+  export default content;
+}
+
+declare module "*.svg" {
+  const content: any;
+  export default content;
+}
+
+declare module "click-outside-vue3";
